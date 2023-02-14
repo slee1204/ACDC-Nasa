a2f4b6e98ccbdb0d60ef49fee6ec0c1c19051641
SueLee <slee1008@my.bcit.ca>
2023-02-14 15:09:49 -0800

    add link to polychromatic page

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import {useState, useEffect} from 'react'
 import axios from 'axios'
+import Link from 'next/link'
 
 
 export default function Home() {
@@ -35,6 +36,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <Link href="/polychromatic">polychromatic</Link>
     {
       data && data.results.map((tech, index)=>{
         return (
